6c5a6388aebaa7a2d2f4eeaa6fc4d7d4ab965386
Charpell <charpellumeh@gmail.com>
2018-08-03 21:51:32 +0100

    Camera Configuration

--- src/profile.js
+++ src/profile.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 class ProfileComponent extends Component {
   constructor(props) {
     super(props);
 
     this.props.navigator.setOnNavigatorEvent(this.navigatorEvent);
+  }
+
+  state = {
+    avatar: ''
   }
   
   navigatorEvent = event => {
@@ -17,11 +22,44 @@ class ProfileComponent extends Component {
     }
   }
 
+  addAvatar = () => {
+    ImagePicker.showImagePicker(null, response => {
+
+      if (response.didCancel) {
+        alert('cancel op')
+      } else if(response.error) {
+        alert('sorry not working')
+      } else {
+        this.setState({
+          avatar: response.uri
+        })
+      }
+    })
+  }
+
   render () {
     return (
-      <Text>This is profile</Text>
+      <View style={{ width: '100%' }}>
+        <Image 
+          source={{ uri: this.state.avatar }}
+          style={ styles.avatar }
+        />
+        <Button 
+          title="Add your avatar"
+          onPress={() => this.addAvatar()}
+        />
+      </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  avatar:{
+      width: '100%',
+      height: 400
+  }
+})
+
 
 export default ProfileComponent;
